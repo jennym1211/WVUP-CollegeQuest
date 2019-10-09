c5f3612a2e4deb420c2cc3b9d2924d6ec22cef8b
Jennifer Mackey <jennym1211@hotmail.com>
2019-10-09 05:38:07 -0400

    update

---
+++ WVUP-TheAdventureGame/Assets/Scripts/ChoiceScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceScript : MonoBehaviour
+{
+    public GameObject TextBox;
+
+    public GameObject ChoiceYes;
+    public GameObject ChoiceNo;
+    public int ChoiceMade;
+    public GameObject AdmissionsButton;
+    public GameObject CareerServicesButton;
+
+    void Start()
+    {
+        AdmissionsButton.SetActive(false);
+        CareerServicesButton.SetActive(false);
+
+    }
+
+    public void ChoiceOptionYes()
+    {
+        TextBox.GetComponent<Text>().text = "Alright, let's head over to the Advising center.";
+        ChoiceMade = 1;
+        AdmissionsButton.SetActive(true);
+    }
+
+
+    public void ChoiceOptionNo()
+    {
+
+        TextBox.GetComponent<Text>().text = "Alright, then let's take a stop to the career center!";
+        ChoiceMade = 2;
+        CareerServicesButton.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ChoiceMade >=1)
+        {
+            ChoiceYes.SetActive(false);
+            ChoiceNo.SetActive(false);
+
+        }
+        
+    }
+}
